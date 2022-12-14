611c8005821f0c3bb087bc3b4bfb4b8664416765
Ryan Barnes <ryan.barnes@uptickhq.com>
2022-12-14 12:15:53 +1100

    feat: Added way to run single day

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
 extern crate core;
 
+use std::env;
+
 mod days;
 mod utils;
 
 fn main() {
-    days::day_1::exec();
-    days::day_2::exec();
-    days::day_3::exec();
-    days::day_4::exec();
-    days::day_5::exec();
-    days::day_6::exec();
-    days::day_7::exec();
-    days::day_8::exec();
-    days::day_9::exec();
-    days::day_10::exec();
-    days::day_11::exec();
-    days::day_12::exec();
+    let args: Vec<String> = env::args().collect();
+    let run_all = args.len() == 1;
+    if run_all | args.contains(&"1".to_string()) {
+        days::day_1::exec();
+    }
+    if run_all | args.contains(&"2".to_string()) {
+        days::day_2::exec();
+    }
+    if run_all | args.contains(&"3".to_string()) {
+        days::day_3::exec();
+    }
+    if run_all | args.contains(&"4".to_string()) {
+        days::day_4::exec();
+    }
+    if run_all | args.contains(&"5".to_string()) {
+        days::day_5::exec();
+    }
+    if run_all | args.contains(&"6".to_string()) {
+        days::day_6::exec();
+    }
+    if run_all | args.contains(&"7".to_string()) {
+        days::day_7::exec();
+    }
+    if run_all | args.contains(&"8".to_string()) {
+        days::day_8::exec();
+    }
+    if run_all | args.contains(&"9".to_string()) {
+        days::day_9::exec();
+    }
+    if run_all | args.contains(&"10".to_string()) {
+        days::day_10::exec();
+    }
+    if run_all | args.contains(&"11".to_string()) {
+        days::day_11::exec();
+    }
+    if run_all | args.contains(&"12".to_string()) {
+        days::day_12::exec();
+    }
 }
